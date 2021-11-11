5a63f993a144cfdd3d56aad87fa814755de2bb61
ian-ludden <iludden94@gmail.com>
2021-11-11 09:12:12 -0600

    Create GerryChain Grid example with auxiliary graph custom updater.

--- definecombine/gerrychain_test.py
+++ definecombine/gerrychain_test.py
@@ -1,44 +1,40 @@
 # Test Gerrychain packages
-from gerrychain import Partition, Graph
 from gerrychain.grid import Grid
 from gerrychain.updaters import cut_edges, Tally
+import networkx as nx
 from random import randint
 
-from .utils import aux_graph
+from utils.aux_graph_updater import aux_graph
 
 num_cols = 10
 num_rows = 10
 votes_per_unit = 10
 
-graph = Graph()
-for i in range(num_rows):
-    for j in range(num_cols):
-        unit_id = i * num_cols + j
-        graph.add_node(
-            unit_id, 
-            init_assignment=(i // 5) * 2 + (j // 5) + 1, 
-            votes_total=votes_per_unit, 
-            votes_definer=randint(1, votes_per_unit)
-            )
+init_assignment_values = [i for i in range(1, num_cols + 1)] * num_rows
+unit_ids = [(i, j) for i in range(num_rows) for j in range(num_cols)]
+init_assignment_dict = {unit_ids[i]: init_assignment_values[i] for i in range(num_rows * num_cols)}
 
-for node in graph.nodes:
-    print(node)
-
-partition = Partition(
-    graph=graph, 
-    assignment="init_assignment", 
-    updaters={"cut_edges": cut_edges, 
-              "votes_definer": Tally("votes_definer"), 
-              "votes_total": Tally("votes_total"), 
-              "aux_graph": aux_graph}
+grid = Grid(
+    dimensions=(num_rows, num_cols), 
+    assignment=init_assignment_dict, 
+    updaters={
+        "cut_edges": cut_edges, 
+        "votes_definer": Tally("votes_definer"), 
+        "votes_total": Tally("votes_total"), 
+        "aux_graph": aux_graph}
     )
 
-print(partition)
-        
-print(partition.graph)
+nx.set_node_attributes(grid.graph, init_assignment_dict, 'init_assignment')
 
-for node in partition.graph:
-    print("Node", node, "is in part", partition.assignment[node])
+nx.set_node_attributes(grid.graph, votes_per_unit, 'votes_total')
 
-print("Number of cut edges:", len(partition.cut_edges))
-print("Auxiliary graph edges:", partition.aux_graph.edges)
+votes_definer_dict = {node: randint(1, votes_per_unit) for node in grid.graph.nodes}
+nx.set_node_attributes(grid.graph, votes_definer_dict, 'votes_definer')
+
+# for unit_id in grid.graph.nodes:
+#         print(unit_id, "is in part", grid.assignment[unit_id])
+#         print(grid.graph.nodes[unit_id])
+
+print(grid.graph)
+print("Number of cut edges:", len(grid.cut_edges))
+print("Auxiliary graph edges:", grid.aux_graph.edges)
--- definecombine/utils/aux_graph_updater.py
+++ definecombine/utils/aux_graph_updater.py
@@ -22,11 +22,11 @@ def aux_graph(partition):
         if aux_edge in H.edges:
             continue
 
-        u_part_votes_definer = partition.votes_for_definer[u_part]
-        v_part_votes_definer = partition.votes_for_definer[v_part]
+        u_part_votes_definer = partition.votes_definer[u_part]
+        v_part_votes_definer = partition.votes_definer[v_part]
         
-        u_part_total_votes = partition.total_votes[u_part]
-        v_part_total_votes = partition.total_votes[v_part]
+        u_part_total_votes = partition.votes_total[u_part]
+        v_part_total_votes = partition.votes_total[v_part]
 
         frac_votes_definer = (u_part_votes_definer + v_part_votes_definer) \
             * 1. / (u_part_total_votes + v_part_total_votes)
@@ -36,6 +36,6 @@ def aux_graph(partition):
         if (frac_votes_definer > 0.5 + tol): edge_weight = 0
         if (abs(frac_votes_definer - 0.5) <= tol): edge_weight = 0.5 
 
-        H.add_edge(aux_edge[0], aux_edge[1], {'weight': edge_weight})
+        H.add_edge(aux_edge[0], aux_edge[1], weight=edge_weight)
     
     return H
