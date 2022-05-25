ef92482aac9a83116c07d9b59e5504daf8f98f5b
ian-ludden <iludden94@gmail.com>
2022-05-25 15:16:53 -0500

    Update DCP high point formulation.

--- definecombine/solve_dcp_high_point.py
+++ definecombine/solve_dcp_high_point.py
@@ -30,14 +30,23 @@ def build_model(unit_ids, unit_weights):
         # unit i is assigned to (the subdistrict centered at) unit j
         x = m.addVars(unit_ids, unit_ids, name="x", vtype=GRB.BINARY)
 
-        # Create variables y_{ij}, indicators for whether 
-        # unit i is assigned to (the district centered at) unit j 
+        # Create variables y_{uv}, indicators for whether 
+        # unit u is a subdistrict center and is assigned to 
+        # (the district centered at) unit v, another subdistrict center
         y = m.addVars(unit_ids, unit_ids, name="y", vtype=GRB.BINARY)
 
-        # Create variables a_{j} and b_{j}, indicators for whether 
-        # the district centered at j is a win for party a (resp., party b)
-        a = m.addVars(unit_ids, name="a", vtype=GRB.BINARY, obj=-1)
-        b = m.addVars(unit_ids, name="b", vtype=GRB.BINARY, obj=1)
+        # Create variables a_{uv} and b_{uv}, indicators for whether 
+        # the district formed by pairing subdistricts centered at u and v 
+        # is a win for party a (resp., party b). 
+        # NB: a_uv = w_uv * y_uv, and b_uv = l_uv * y_uv
+        a = m.addVars(unit_ids, unit_ids, name="a", vtype=GRB.BINARY)
+        b = m.addVars(unit_ids, unit_ids, name="b", vtype=GRB.BINARY)
+
+        # Create variables w_{uv} and l_{uv}, indicators for whether 
+        # the district formed by pairing subdistricts centered at u and v 
+        # is a win for party a (resp., party b)
+        w = m.addVars(unit_ids, unit_ids, name="w", vtype=GRB.BINARY)
+        l = m.addVars(unit_ids, unit_ids, name="l", vtype=GRB.BINARY)
 
         # Create edges for grid graph
         x_edges = set()
@@ -81,6 +90,7 @@ def build_model(unit_ids, unit_weights):
     
 
         # Add constraint: force all assignments in x and y to go from lower units to higher units
+        # (breaks some symmetry)
         for i in unit_ids:
             for j in unit_ids:
                 if j < i:
@@ -91,44 +101,52 @@ def build_model(unit_ids, unit_weights):
         # Add constraint: sum of x_{jj} = NUM_SUBDISTRICTS
         m.addConstr(sum(x[i, i] for i in unit_ids) == NUM_SUBDISTRICTS, "num_subdistricts")
 
+
         # # Add constraints: every unit fully assigned
         m.addConstrs(
             (x.sum(i, '*') == 1 
                 for i in unit_ids), "fullyassigned")
 
+
         # Add constraints: every subdistrict has exactly NUM_UNITS / NUM_SUBDISTRICTS units (perfect population balance)
         for j in unit_ids:
             m.addConstr(sum(x[i, j] for i in unit_ids) == UNITS_PER_SUBDISTRICT * x[j, j], "subdistrict size %s" % (j))
 
-        # Add constraints: every district has exactly NUM_UNITS / NUM_DISTRICTS units (perfect population balance)
-        for j in unit_ids:
-            m.addConstr(sum(y[i, j] for i in unit_ids) == UNITS_PER_DISTRICT * y[j, j], "district size %s" % (j))
+
+        # # Add constraints: every district has exactly NUM_UNITS / NUM_DISTRICTS units (perfect population balance)
+        # for j in unit_ids:
+        #     m.addConstr(sum(y[i, j] for i in unit_ids) == UNITS_PER_DISTRICT * y[j, j], "district size %s" % (j))
+
 
         # Add constraints: can only assign to centers
         for i in unit_ids:
             for j in unit_ids:
                 if i == j: continue; # Only need this constraint for distinct i, j
                 m.addConstr(x[i, j] - x[j, j] <= 0, "onlycenters[%s, %s]" % (i, j))
-        
+
+
         # Add constraints: 
-        # a_{j} is 1 if and only if definer, party a, wins district centered at j;
-        # b_{j} is 1 if and only if combiner, barty b, wins district centered at j. 
-        for j in unit_ids:
-            m.addConstr(sum(y[i, j] * unit_weights[i] 
-                for i in unit_ids) <= UNITS_PER_DISTRICT * a[j], 
-                "a def1 %s" % (j))
-        
-            m.addConstr(sum(y[i, j] * unit_weights[i]
-                for i in unit_ids) >= 1 - (UNITS_PER_DISTRICT + 1) * (1 - a[j]), 
-                "a def2 %s" % (j))
+        # a_{uv} is 1 if and only if definer, party a, wins district formed from 
+        # subdistricts centered at u and v;
+        # b_{uv} is 1 if and only if combiner, party b, wins district formed from 
+        # subdistricts centered at u and v.
+        for u in unit_ids:
+            for v in unit_ids:
+                m.addConstr(sum((x[i, u] + x[i, v]) * unit_weights[i] 
+                    for i in unit_ids) <= UNITS_PER_DISTRICT * w[u, v], 
+                    "w def1 %s %s" % (u, v))
+            
+                m.addConstr(sum((x[i, u] + x[i, v]) * unit_weights[i]
+                    for i in unit_ids) >= 1 - (UNITS_PER_DISTRICT + 1) * (1 - w[u, v]), 
+                    "w def2 %s %s" % (u, v))
 
-            m.addConstr(sum(y[i, j] * unit_weights[i] * (-1)
-                for i in unit_ids) <= UNITS_PER_DISTRICT * b[j], 
-                "b def1 %s" % (j))
+                m.addConstr(sum((x[i, u] + x[i, v]) * unit_weights[i] * (-1)
+                    for i in unit_ids) <= UNITS_PER_DISTRICT * l[u, v], 
+                    "l def1 %s %s" % (u, v))
 
-            m.addConstr(sum(y[i, j] * unit_weights[i] * (-1)
-                for i in unit_ids) >= 1 - (UNITS_PER_DISTRICT + 1) * (1 - b[j]), 
-                "b def2 %s" % (j))
+                m.addConstr(sum((x[i, u] + x[i, v]) * unit_weights[i] * (-1)
+                    for i in unit_ids) >= 1 - (UNITS_PER_DISTRICT + 1) * (1 - l[u, v]), 
+                    "l def2 %s %s" % (u, v))
 
         
         # Add flow-based (SHIR) contiguity constraints (alternative is separator-based, CUT, from Lykhovyd, Validi & Buchanan 2021)
@@ -154,6 +172,18 @@ def build_model(unit_ids, unit_weights):
                     m.addConstr(c[i, j, k, ell] <= x[j, ell], "c def 2 %s %s %s %s" % (i, j, k, ell))
                     m.addConstr(c[i, j, k, ell] >= x[i, k] + x[j, ell] - 1, "c def 3 %s %s %s %s" % (i, j, k, ell))
 
+
+        # Add constraints for a and b definitions
+        for u in unit_ids:
+            for v in unit_ids:
+                m.addConstr(a[u, v] <= y[u, v], "a def 1 %s %s" % (u, v))
+                m.addConstr(a[u, v] <= w[u, v], "a def 2 %s %s" % (u, v))
+                m.addConstr(a[u, v] >= w[u, v] + y[u, v] - 1, "a def 3 %s %s" % (u, v))
+                m.addConstr(b[u, v] <= y[u, v], "b def 1 %s %s" % (u, v))
+                m.addConstr(b[u, v] <= l[u, v], "b def 2 %s %s" % (u, v))
+                m.addConstr(b[u, v] >= l[u, v] + y[u, v] - 1, "b def 3 %s %s" % (u, v))
+
+
         # Add constraints for both-units-are-subdistrict-centers variables
         for k in unit_ids:
             for ell in unit_ids:
@@ -162,34 +192,34 @@ def build_model(unit_ids, unit_weights):
                 m.addConstr(z[k, ell] <= x[ell, ell], "z def 2 %s %s" % (k, ell))
                 m.addConstr(z[k, ell] >= x[k, k] + x[ell, ell] - 1, "z def 3 %s %s" % (k, ell))
 
-        # Add constraints: y centers follow x centers
-        for j in unit_ids:
-            m.addConstr(y[j, j] <= x[j, j], "y center only if x center %s" % (j))
-        
-        # Add constraint: NUM_DISTRICTS total y centers
-        m.addConstr(sum(y[j, j] for j in unit_ids) == NUM_DISTRICTS, "num_districts")
-        
-        # Add constraints: y assignments only to centers
-        for j in unit_ids:
-            for i in unit_ids:
-                m.addConstr(y[i, j] <= y[j, j], "y assign only to centers %s %s" % (i, j))
 
-        # Add constraints: each unit assigned in y
-        for i in unit_ids:
-            m.addConstr(sum(y[i, j] for j in unit_ids) == 1, "y assign unit %s to exactly one center" % (i))
-        
-        # Add constraints: if x_{ij} = 1, then y_{ik} <= y_{jk} for all k (only need for k not equal to i nor j)
-        for i in unit_ids:
-            for j in unit_ids:
-                for k in unit_ids:
-                    if k == i or k == j: continue
-                    m.addConstr(y[i, k] <= y[j, k] + (1 - x[i, j]), "subdistricts follow centers %s %s %s" % (i, j, k))
-        
+        # Add constraint: NUM_DISTRICTS total y edges matched (pairs of subdistricts)
+        m.addConstr(sum(y[u, v] for u in unit_ids for v in unit_ids) == NUM_DISTRICTS, "num_districts")
+
+
+        # Add constraints: y_uv only if x_uu x_vv, and y_uv = 0 for u >= v
+        for u in unit_ids:
+            for v in unit_ids:
+                if u >= v:
+                    m.addConstr(y[u, v] == 0, "Fix y_%d%d = 0" % (u, v))
+                else:
+                    m.addConstr(y[u, v] <= z[u, v], "y_%d%d only if both are subdistrict centers" % (u, v))
+
+
+        # Add constraints: each subdistrict center matched at most once in y
+        for u in unit_ids:
+            m.addConstr(sum(y[u, v] + y[v, u] for v in unit_ids) <= 1, "y unit %d participates in at most one matching edge" % (u))
+
+
         # Add constraints: subdistrict center assigned to subdistrict center requires at least one cut-edge between subdistricts
-        for k in unit_ids:
-            for ell in unit_ids:
-                if ell == k: continue
-                m.addConstr(y[k, ell] <= (sum(c[i,j,k,ell] for i,j in all_directed_edges)) + (1 - z[k, ell]), "adjacent subdistricts %s %s" % (k, ell))
+        for u in unit_ids:
+            for v in unit_ids:
+                if u >= v: continue
+                m.addConstr(y[u, v] <= (sum(c[i,j,u,v] for i,j in all_directed_edges)), "adjacent subdistricts %s %s" % (u, v))
+
+
+        # Set objective: maximize sum a_uv minus b_uv
+        m.setObjective(sum(a[u, v] - b[u, v] for u in unit_ids for v in unit_ids), sense=GRB.MAXIMIZE)
         
         return m
 
@@ -207,7 +237,7 @@ def summarize_model_results(m):
     # Check which units are subdistrict centers, and extract subdistrict partition
     partition_dict = {}
     assignment_dict = {}
-    # tol = m.Params.IntFeasTol
+    tol = m.Params.IntFeasTol
 
     x_vals = np.zeros((NUM_UNITS, NUM_UNITS))
 
@@ -222,6 +252,7 @@ def summarize_model_results(m):
     # Assign each unit to the unit with max weight in x
     assignments = np.argmax(x_vals, axis=1)
     print(assignments)
+    print(np.unique(assignments))
 
     assignment_dict = {unit_id: assignments[unit_id] for unit_id in unit_ids}
     for subdistrict_center in np.unique(assignments):
@@ -236,17 +267,23 @@ def summarize_model_results(m):
 
     y_vals = np.zeros((NUM_UNITS, NUM_UNITS))
 
-    for v in m.getVars():
-        var_name_parts = re.split('\[|,|\]', v.varName)
+    for var in m.getVars():
+        var_name_parts = re.split('\[|,|\]', var.varName)
         if not (var_name_parts[0] == 'y'): continue
-        i = int(var_name_parts[-3])
-        j = int(var_name_parts[-2])
+        u = int(var_name_parts[-3])
+        v = int(var_name_parts[-2])
 
-        y_vals[i, j] = v.x
+        if var.x >= 1 - tol: # v is a district center
+            print(var.varName, '=', var.x)
+            y_vals[v, v] = 1
+            for i in unit_ids:
+                if (assignment_dict[i] == u) or (assignment_dict[i] == v):
+                    y_vals[i, v] = 1
     
-    # Assign each unit to the unit with max weight in y
+    # # Assign each unit to the unit with max weight in y
     y_assignments = np.argmax(y_vals, axis=1)
     print(y_assignments)
+    # print(np.unique(y_assignments))
 
     y_assignment_dict = {unit_id: y_assignments[unit_id] for unit_id in unit_ids}
     for district_center in np.unique(y_assignments):
@@ -263,14 +300,14 @@ def summarize_model_results(m):
         for unit_id in y_partition_dict[key]:
             district_weight += unit_weights[unit_id]
         print('District centered at unit', key, 'has total weight', district_weight)
-        if district_weight > 0:
+        if district_weight >= 0:
             count_a_wins += 1
-        if district_weight < 0:
+        if district_weight <= 0:
             count_b_wins += 1
         
-    print('Wins for a:\t', count_a_wins)
-    print('Wins for b:\t', count_b_wins)
-    print('Ties:      \t', NUM_DISTRICTS - count_a_wins - count_b_wins)
+    print('Wins/ties for a:\t', count_a_wins)
+    print('Wins/ties for b:\t', count_b_wins)
+    print('Net:', count_a_wins - count_b_wins)
 
     # Print visualization of solution (district plan) using letters for districts
     centers = sorted([key for key in y_partition_dict])
@@ -335,12 +372,13 @@ if __name__ == '__main__':
         m.optimize()
         summarize_model_results(m)
 
-        print("\n*** RESET and RESOLVE ***\n")
-        m.reset()
-        m.addConstr(m.getVarByName("x[0,6]") == 0, "forced_zero_x_{}_{}".format(0, 6))
+        # print("\n*** RESET, RELAX, and RESOLVE ***\n")
+        # m.reset()
+        # m = m.relax()
+        # # m.addConstr(m.getVarByName("x[0,6]") == 0, "forced_zero_x_{}_{}".format(0, 6))
         
-        m.optimize()
-        summarize_model_results(m)
+        # m.optimize()
+        # summarize_model_results(m)
 
     except gp.GurobiError as e:
         print('Error code ' + str(e.errno) + ': ' + str(e))
