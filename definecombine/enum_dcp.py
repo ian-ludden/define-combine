23188779b53fb0d030332e69e5d0699805725785
ian-ludden <iludden94@gmail.com>
2022-10-20 14:54:05 -0500

    Remove MIP attempts.

--- definecombine/enum_dcp.py
+++ definecombine/enum_dcp.py
@@ -40,7 +40,7 @@ def solve_dcp_grid(num_rows, num_cols, num_districts, voter_grid):
 
     with open(enumeration_filename, 'r') as enum_file:
         csv_enum_file = csv.reader(enum_file)
-        for line in tqdm(csv_enum_file, desc="Trying all subdistrict partitions..."):
+        for line in tqdm(csv_enum_file, desc="Trying all subdistrict partitions"):
             num_partitions += 1
             assignment_grid = np.array(line, dtype=int).reshape(num_rows, num_cols)
 
