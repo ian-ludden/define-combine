e4a6ce8ce54673feee7bfad42d6529dea0a433d1
Ian Ludden <luddenig@rose-hulman.edu>
2024-01-18 10:13:12 -0500

    Add MibS solver examples, both with Julia/BilevelJuMP (preferred, working smoothly) and Pyomo/PAO (not working yet, maybe due to MibS installation issues.)

---
+++ definecombine/mibs_test.jl
@@ -0,0 +1,209 @@
+# # MibS: Example 1 (Experimental feature)
+
+# Model of the problem
+# First level
+# ```math
+# \min_{x} -3x -7y,\\
+# \notag s.t.\\
+# -3x + 2y \leq 12,\\ 
+# x + 2y \leq 20,\\
+# x \leq 10,\\
+# x \in \mathbb{Z},\\
+# ```
+# Second level
+# ```math
+# \min_{y} y,\\
+# \notag s.t.\\
+# 2x - y <= 7,\\
+# -2x + 4y <= 16,\\
+# y <= 5\\
+# y \in \mathbb{Z}\\
+# ```
+
+using BilevelJuMP
+using Printf
+using Test
+using MibS_jll
+
+function test_example_one()
+    # Taken from https://github.com/joaquimg/BilevelJuMP.jl/blob/master/docs/src/examples/MibS_example1.jl
+    model = BilevelModel()
+
+    # First we need to create all of the variables in the upper and lower problems:
+    
+    # Upper level variables
+    @variable(Upper(model), x, Int)
+    
+    #Lower level variables
+    @variable(Lower(model), y, Int)
+    
+    # Then we can add the objective and constraints of the upper problem:
+    
+    # Upper level objecive function
+    @objective(Upper(model), Min, -3x - 7y)
+    
+    # Upper constraints
+    @constraints(Upper(model), begin
+        u1, -3x + 2y <= 12
+        u2, x + 2y <= 20
+        u3, x <= 10
+    end)
+    
+    # Followed by the objective and constraints of the lower problem:
+    
+    # Lower objective function
+    @objective(Lower(model), Min, y)
+    
+    # Lower constraints
+    @constraint(Lower(model), l1, 2x - y <= 7)
+    @constraint(Lower(model), l2, -2x + 4y <= 16)
+    @constraint(Lower(model), l3, y <= 5)
+    
+    # Using MibS Solver
+    solution = BilevelJuMP.solve_with_MibS(model, MibS_jll.mibs)
+    @printf "Status: %s\n" solution.status
+    @printf "Opt obj.: %.1f\n" solution.objective
+    @printf "x value: %.1f\n" solution.all_upper["x"]
+    @printf "y value: %.1f\n" solution.all_lower["y"]
+    
+    @printf "\nUpper variables:\n"
+    for var ∈ solution.all_upper
+        @printf "var: %s\n" var
+    end
+    
+    @printf "\nLower variables:\n"
+    for var ∈ solution.all_lower
+        @printf "var: %s\n" var
+    end
+    
+    # Auto testing
+    @test solution.status == true
+    @test solution.objective ≈ -53
+    @test solution.nonzero_upper == Dict(0 => 6.0)
+    @test solution.nonzero_lower == Dict(0 => 5.0)
+    @test solution.all_upper["x"] == 6.0
+    @test solution.all_lower["y"] == 5.0
+    return 0
+end
+
+function test_example_two()
+    # Same problem as in build_sample_model_two of pyomo_pao_example.py, 
+    # Example 1 of Moore + Bard (1990)
+    
+    model = BilevelModel()
+    # Upper level variables
+    @variable(Upper(model), x, Int)
+    
+    #Lower level variables
+    @variable(Lower(model), y, Int)
+    
+    # Upper level objecive function
+    @objective(Upper(model), Max, x + 10y)
+    
+    # Upper constraints
+    @constraints(Upper(model), begin
+        u1, x >= 0
+    end)
+    
+    # Lower objective function
+    @objective(Lower(model), Max, -y)
+    
+    # Lower constraints
+    @constraint(Lower(model), l1, -25x +20y <= 30)
+    @constraint(Lower(model), l2, x + 2y <= 10)
+    @constraint(Lower(model), l3, 2x - y <= 15)
+    @constraint(Lower(model), l4, 2x + 10y >= 15)
+    @constraint(Lower(model), l5, y >= 0)
+    
+    # Using MibS Solver
+    solution = BilevelJuMP.solve_with_MibS(model, MibS_jll.mibs)
+    println(solution)
+    @printf "Status: %s\n" solution.status
+    @printf "Opt obj.: %.1f\n" solution.objective
+    @printf "x value: %.1f\n" solution.all_upper["x"]
+    @printf "y value: %.1f\n" solution.all_lower["y"]
+    
+    @printf "\nUpper variables:\n"
+    for var ∈ solution.all_upper
+        @printf "var: %s\n" var
+    end
+    
+    @printf "\nLower variables:\n"
+    for var ∈ solution.all_lower
+        @printf "var: %s\n" var
+    end
+    
+    # Auto testing
+    @test solution.status == true
+    @test solution.objective ≈ -22.000000000001
+    # @test solution.nonzero_upper == Dict(0 => 6.0)
+    # @test solution.nonzero_lower == Dict(0 => 5.0)
+    @test solution.all_upper["x"] == 2.0
+    @test solution.all_lower["y"] == 2.0
+    return 0
+end
+
+
+function test_example_three()
+    # Same problem as in build_sample_model_three of pyomo_pao_example.py, 
+    # from PAO examples (Bard 1998, 5.1.1): 
+    # https://pao.readthedocs.io/en/latest/examples.html
+    
+    model = BilevelModel()
+    # Upper level variables
+    @variable(Upper(model), x, Int) # Default type is Real
+    
+    #Lower level variables
+    @variable(Lower(model), y, Int) # Default type is Real
+    
+    # Upper level objecive function
+    @objective(Upper(model), Min, x - 4y)
+    
+    # Upper constraints
+    @constraints(Upper(model), begin
+        u1, x >= 0
+    end)
+    
+    # Lower objective function
+    @objective(Lower(model), Min, y)
+    
+    # Lower constraints
+    @constraint(Lower(model), l1, -x -y <= -3)
+    @constraint(Lower(model), l2, -2x + y <= 0)
+    @constraint(Lower(model), l3, 2x + y <= 12)
+    @constraint(Lower(model), l4, 3x - 2y <= 4)
+    @constraint(Lower(model), l5, y >= 0)
+    
+    # Using MibS Solver
+    solution = BilevelJuMP.solve_with_MibS(model, MibS_jll.mibs)
+    println(solution)
+    @printf "Status: %s\n" solution.status
+    @printf "Opt obj.: %.1f\n" solution.objective
+    @printf "x value: %.1f\n" solution.all_upper["x"]
+    @printf "y value: %.1f\n" solution.all_lower["y"]
+    
+    @printf "\nUpper variables:\n"
+    for var ∈ solution.all_upper
+        @printf "var: %s\n" var
+    end
+    
+    @printf "\nLower variables:\n"
+    for var ∈ solution.all_lower
+        @printf "var: %s\n" var
+    end
+    
+    # Auto testing
+    @test solution.status == true
+    @test solution.objective ≈ -12.000000000001
+    # @test solution.nonzero_upper == Dict(0 => 6.0)
+    # @test solution.nonzero_lower == Dict(0 => 5.0)
+    @test solution.all_upper["x"] == 4.0
+    @test solution.all_lower["y"] == 4.0
+    return 0
+end
+    
+# test_example_one()
+# println("\n")
+# test_example_two()
+# println("\n")
+test_example_three()
--- definecombine/pyomo_pao_example.py
+++ definecombine/pyomo_pao_example.py
@@ -25,10 +25,10 @@ def build_sample_model_one():
 
 def build_sample_model_two():
     M = pe.ConcreteModel()
-    M.x = pe.Var(domain=pe.NonNegativeIntegers)
+    M.x = pe.Var()#bounds=(0,None))#domain=pe.NonNegativeIntegers)
 
     M.L = pao.pyomo.SubModel(fixed=[M.x])
-    M.L.y = pe.Var(domain=pe.NonNegativeIntegers)
+    M.L.y = pe.Var()#bounds=(0,None))#domain=pe.NonNegativeIntegers)
 
     M.o = pe.Objective(expr=M.x + 10*M.L.y, sense=pe.maximize)
     
@@ -41,14 +41,61 @@ def build_sample_model_two():
     return M
 
 
+def build_sample_model_three():
+    M = pe.ConcreteModel()
+    M.x = pe.Var(bounds=(0,None))
+    M.y = pe.Var(bounds=(0,None))
+
+    M.o = pe.Objective(expr=M.x - 4*M.y)
+
+    M.L = pao.pyomo.SubModel(fixed=M.x)
+    M.L.o = pe.Objective(expr=M.y)
+    M.L.c1 = pe.Constraint(expr=   -M.x -   M.y <= -3)
+    M.L.c2 = pe.Constraint(expr= -2*M.x +   M.y <=  0)
+    M.L.c3 = pe.Constraint(expr=  2*M.x +   M.y <= 12)
+    M.L.c4 = pe.Constraint(expr=  3*M.x - 2*M.y <=  4)
+
+    return M
+    
+
 if __name__ == '__main__':
-    M = build_sample_model_one()
-    opt = pao.Solver(SOLVER_NAME)
+    # M = build_sample_model_one()
+    # opt = pao.Solver(SOLVER_NAME)
     # results = opt.solve(M)
+    # print(results)
     # print(M.x.value, M.y.value, M.L.z.value)
 
-    M = build_sample_model_two()
-    opt = pao.Solver(SOLVER_NAME)
-    # results = opt.solve(M)
-    # print(M.x.value, M.L.y.value)
+    # M2 = build_sample_model_two()
+    # opt = pao.Solver(SOLVER_NAME)
+    # results2 = opt.solve(M2)
+    # print(results2)
+    # print(M2.x.value, M2.L.y.value)
+
+    M = pe.ConcreteModel("Name of model.")
+    M.x = pe.Var(bounds=(0,None))
+    M.y = pe.Var(bounds=(0,None))
+
+    M.o = pe.Objective(expr=M.x - 4*M.y)
+
+    M.L = pao.pyomo.SubModel(fixed=M.x)
+    M.L.o = pe.Objective(expr=M.y)
+    M.L.c1 = pe.Constraint(expr=   -M.x -   M.y <= -3)
+    M.L.c2 = pe.Constraint(expr= -2*M.x +   M.y <=  0)
+    M.L.c3 = pe.Constraint(expr=  2*M.x +   M.y <= 12)
+    M.L.c4 = pe.Constraint(expr=  3*M.x - 2*M.y <=  4)
+
+    print(M)
+    for v in [M.x, M.y]:
+        print(v.value)
+
+    M.pprint()
+    
+    
+    with pao.Solver(SOLVER_NAME) as solver:
+        print(type(solver), solver)
+        results3 = solver.solve(M)
+        print(results3.solver.termination_condition)
+        print(results3)
+        print("Optimal termination?", results3.check_optimal_termination())
+        print("x = ", M.x.value, ", y = ", M.y.value, sep='')
 
