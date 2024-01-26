d938745dc5a9128d7c861ea36e3529125438a941
Ian Ludden <luddenig@rose-hulman.edu>
2024-01-26 11:30:44 -0500

    Continue building bilevel MILP model in dcp_small_mibs.

--- definecombine/dcp_small_mibs.jl
+++ definecombine/dcp_small_mibs.jl
@@ -26,6 +26,43 @@ function rowcol_to_index(row, col, num_cols)
 end
 
 """
+Return all ordered pairs of edges, 
+using indices starting at 0 in the top-left corner and 
+moving left-to-right, top-to-bottom, 
+in a grid graph with the given number of rows and columns. 
+"""
+function get_directed_grid_edges(num_rows, num_cols)
+    undir_edges = Vector{Tuple{Int, Int}}()
+
+    for i = 0:num_rows-1
+        for j = 0:num_cols-1
+            current = rowcol_to_index(i, j, num_cols)
+
+            # Right edge
+            right_neighbor = current + 1
+            if j + 1 < num_cols
+                push!(undir_edges, (current + 1, right_neighbor + 1))
+            end
+
+            # Down edge
+            down_neighbor = current + num_cols
+            if i + 1 < num_rows
+                push!(undir_edges, (current + 1, down_neighbor + 1))
+            end
+        end
+    end
+
+    dir_edges = Vector{Tuple{Int, Int}}()
+    for i = 1:length(undir_edges)
+        u, v = undir_edges[i]
+        push!(dir_edges, (u, v))
+        push!(dir_edges, (v, u))
+    end
+
+    return dir_edges
+end
+
+"""
 Assuming a `num_rows` by `num_cols` grid of units, 
 populate a matrix (2D array) representing all pairwise 
 squared Euclidean distances between unit centers. 
@@ -59,7 +96,7 @@ Returns new positions (pos[i] is the new position of unit i)
 and the size of the set B. 
 """
 function shuffle_indices(num_rows, num_cols)
-    supported_dimensions = [(20, 20), (10, 10), (8, 8)] # , (4, 4)
+    supported_dimensions = [(20, 20), (10, 10), (8, 8), (4, 4)]
     
     num_districts = 4 # TODO: Allow other numbers of districts
     
@@ -82,6 +119,9 @@ function shuffle_indices(num_rows, num_cols)
     elseif num_rows == 8
         rows_in_B = [0, 1, 2, 4, 5, 6]
         rows_not_in_B = [3, 7]
+    elseif num_rows == 4
+        rows_in_B = [0, 2]
+        rows_not_in_B = [1, 3]
     end
     println([r for r in rows_in_B])
     cols_in_B = rows_in_B # By symmetry; would need to change to support asymmetric grid dimensions
@@ -108,18 +148,112 @@ function shuffle_indices(num_rows, num_cols)
     return pos, length(pairs_in_B)
 end
 
+"""
+Build a bilevel MILP model for the define-combine procedure 
+on a grid graph with the given number of rows, columns, and districts. 
+
+Then, solve the model using MibS and return the solution. 
+
+Note: The `voter_grid` parameter is a 2D float array of units'
+definer fractional vote-shares (between 0.0 and 1.0). 
+"""
+function build_and_solve_grid_model(num_rows, num_cols, num_districts, voter_grid)
+    num_units = num_rows * num_cols 
+    epsilon = 1.0e-8 # Needs to be smaller than smallest possible fractional part of a district's net votes
+    
+    # Create model, x (definer assignment), and y (combiner assignment) variables
+    m = BilevelModel()
+    # @variable(Upper(m), x[1:num_units, 1:num_units], Bin)
+    # @variable(Lower(m), y[1:num_units, 1:num_units], Bin)
+
+    # Add objectives: definer's net utility, combiner's net utility. 
+    # Need new variables, a_j for definer win and b_j for combiner win. 
+    @variable(Lower(m), a[1:num_units], Bin)
+    @variable(Lower(m), b[1:num_units], Bin)
+    for i = 1:num_units
+        @constraint(Lower(m), a[i] + b[i] <= 1)
+    end
+    @objective(Upper(m), Max, sum(a) - sum(b))
+    @objective(Lower(m), Max, sum(b) - sum(a))
+
+    # Add big-M constraints for defining a_j and b_j using voter_grid and y_ij
+    # TODO
+
+    # Add sum(x_jj) <= 2 * num_districts (only 2N subdistrict centers)
+    # @constraint(Upper(m), sum(x[j, j] for j in 1:num_units) <= 2 * num_districts)
+    
+    # Add y_jj <= x_jj constraints (y centers must also be x centers)
+    # for j = 1:num_units
+    #     @constraint(Lower(m), y[j, j] <= x[j, j])
+    # end
+
+    # Add constraints that y assignments follow x assignments: 
+    # If x_ij = 1, then y_ik <= y_jk for all units i, j, k. 
+    # for k = 1:num_units
+    #     for i = 1:num_units
+    #         for j = 1:num_units
+    #             @constraint(Lower(m), y[i, k] <= y[j, k] + (1 - x[i, j]))
+    #         end
+    #     end
+    # end
+
+    # Get directed edge set
+    dir_edge_list = get_directed_grid_edges(num_rows, num_cols)
+    num_dir_edges = length(dir_edge_list)
+    # Get undirected edges (u, v) in canonical order (u < v)
+    undir_edge_list = [(e[1], e[2]) for e in dir_edge_list if e[1] < e[2]]
+    num_undir_edges = length(undir_edge_list)
+    @printf "Directed edges:  \t %i\n" num_dir_edges
+    @printf "Undirected edges:\t %i\n" num_undir_edges
+    # display(undir_edge_list)
+    println()
+
+    # Add Shirabe flow variables for contiguity
+    # @variable(Upper(m), f[1:num_units, 1:num_dir_edges] >= 0.0)
+
+    # Add cut edge variables with quadratic constraints to define them
+    # TODO: May have to manually linearize these quadratic constraints if MibS is unhappy
+    # @variable(Upper(m), c[1:num_undir_edges, 1:num_units, 1:num_units], Bin)
+    # for (index, e) in enumerate(undir_edge_list)
+    #     for k = 1:num_units
+    #         for ell = 1:num_units
+    #             @constraint(Upper(m), c[index, k, ell] == x[e[1], k] * x[e[2], ell])
+    #         end
+    #     end
+    # end
+
+    # Add constraints linking y with x via c
+    # for k = 1:num_units
+    #     for ell = 1:num_units
+    #         @constraint(Lower(m), y[k, ell] <= sum(c[e, k, ell] for e in 1:num_undir_edges))
+    #     end
+    # end
+    
+    return m
+end
 
 
 
 index = 11
-num_rows = 8
-num_cols = 8
+num_rows = 4
+num_cols = 4
+num_districts = 4
+voter_grid = zeros(Float64, 4, 4)
+voter_grid[1:2, 1:4] .= 1.0
 
-d2 = populate_squared_distance_matrix(num_rows, num_cols)
-display(d2)
+# d2 = populate_squared_distance_matrix(num_rows, num_cols)
+# display(d2)
 
 row, col = index_to_rowcol(index, num_cols)
 @printf "\nrow = %i, col = %i\n" row col
 
 pos, size_of_B  = shuffle_indices(num_rows, num_cols)
 @printf "\nL-fixing: B contains %i units.\n" size_of_B
+
+model = build_and_solve_grid_model(num_rows, num_cols, num_districts, voter_grid)
+display(model)
+
+solution = BilevelJuMP.solve_with_MibS(model, MibS_jll.mibs, verbose_results=true)
+# println(solution)
+@printf "Status: %s\n" solution.status
+@printf "Opt obj.: %.1f\n" solution.objective
