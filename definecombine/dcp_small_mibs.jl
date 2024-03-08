5865e636ead57b4916315d44458ad3daaba9c9d6
Ian Ludden <luddenig@rose-hulman.edu>
2024-03-08 12:53:22 -0500

    Document BilevelJuMP bug with variable arrays using MibS solver.

--- definecombine/dcp_small_mibs.jl
+++ definecombine/dcp_small_mibs.jl
@@ -7,10 +7,6 @@ using MibS_jll
 using Printf
 using Test
 
-ENV["TMPDIR"] = joinpath(pwd(), "temporary")
-mkdir(ENV["TMPDIR"])
-mktempdir()
-
 """
 Convert index of a grid cell to (row, col) pair. 
 Index starts at 0 in top-left corner and increases 
@@ -193,8 +189,8 @@ function build_and_solve_grid_model(num_rows, num_cols, num_districts, voter_gri
     # end
     # @objective(Upper(m), Max, sum(a) - sum(b))
     # @objective(Lower(m), Max, sum(b) - sum(a))
-    @objective(Upper(m), Min, sum(y[j, j] for j in 1:num_units))
-    @objective(Lower(m), Max, sum(y[j, j] for j in 1:num_units))
+    @objective(Upper(m), Min, 4)#Max, sum(x[j, j] for j in 1:num_units))
+    @objective(Lower(m), Min, 17)#Max, sum(y[j, j] for j in 1:num_units))
 
     # Add big-M constraints for defining a_j and b_j using voter_grid and y_ij
     # for j = 1:num_units
@@ -212,52 +208,143 @@ function build_and_solve_grid_model(num_rows, num_cols, num_districts, voter_gri
     # end
 
     # Add sum(x_jj) <= 2 * num_districts (only 2N subdistrict centers)
-    @constraint(Upper(m), sum(x[j, j] for j in 1:num_units) <= 2 * num_districts)
+    @constraint(Upper(m), c_num_subdist_ub, sum(x[j, j] for j in 1:num_units) <= 2 * num_districts)
+    @constraint(Upper(m), c_num_subdist_lb, sum(x[j, j] for j in 1:num_units) >= 2 * num_districts)
+    println(c_num_subdist_ub)
+    println(c_num_subdist_lb)
     
     # Add y_jj <= x_jj constraints (y centers must also be x centers)
-    for j = 1:num_units
-        @constraint(Lower(m), y[j, j] <= x[j, j])
+    # for j = 1:num_units
+    #     @constraint(Lower(m), y[j, j] <= x[j, j])
+    # end
+
+    # Add sum(y_jj) == num_districts (exactly N district centers)
+    @constraint(Lower(m), c_num_dist_ub, sum(y[j, j] for j in 1:num_units) <= 2) # num_districts
+    @constraint(Lower(m), c_num_dist_lb, sum(y[j, j] for j in 1:num_units) >= 2) # num_districts
+    println(c_num_dist_ub)
+    println(c_num_dist_lb)
+    
+    println("Are these constraints being ignored?!?")
+    # @constraint(Lower(m), c_center3, y[3, 3] <= 0)
+    # println(c_center3)
+
+    i1 = 2
+    i2 = 5
+    @constraint(Lower(m), c_only_centers, y[i1, i1] + y[i2, i2] >= 2)
+    println(c_only_centers) 
+    
+    # @constraint(Lower(m), c_center3_2, y[3, 3] >= y[5, 2])
+    # println(c_center3_2)
+    
+    # @constraint(Lower(m), y[5, 5] <= 0)
+    # @constraint(Lower(m), y[7, 7] <= 0)
+    # @constraint(Lower(m), y[9, 9] <= 0)
+    # @constraint(Lower(m), y[11, 11] >= 1)
+    println("No, but the indices seem to be changing...")
+
+    # @constraint(Lower(m), y[5, 2] <= 0) # No noticeable effect for these four
+    # @constraint(Lower(m), y[10, 13] <= 0)
+    # @constraint(Lower(m), y[1, 11] <= 0)
+    # @constraint(Lower(m), y[16, 9] <= 0)
+    # @constraint(Lower(m), y[4, 13] <= 0)
+
+    for i = 1:num_units
+        if i != i1 && i != i2
+            @constraint(Lower(m), y[i, i] <= 0)
+        end
     end
+
+    # TEMP (TODO: delete): add constraints forcing all variables to zero except (sub)district centers
+    # for i = 1:num_units
+    #     for j = 1:num_units
+    #         if i != j
+    #             @constraint(Upper(m), x[i, j] <= 0)
+    #             @constraint(Lower(m), y[i, j] <= 0)
+    #         end
+    #     end
+    # end
 
     # Add assignment constraints: every unit assigned exactly once
-    for i = 1:num_units
-        @constraint(Upper(m), sum(x[i, j] for j in 1:num_units) == 1)
-        @constraint(Lower(m), sum(y[i, j] for j in 1:num_units) == 1)
-    end
+    # c_x_asst_ub = [
+    #     @constraint(Upper(m), sum(x[i, j] for j in 1:num_units) <= 1)
+    #     for i = 1:num_units
+    # ]
+    # c_x_asst_lb = [
+    #     @constraint(Upper(m), sum(x[i, j] for j in 1:num_units) >= 1)
+    #     for i = 1:num_units
+    # ]
+    # c_y_asst_ub = [
+    #     @constraint(Lower(m), sum(y[i, j] for j in 1:num_units) <= 1)
+    #     for i = 1:num_units
+    # ]
+    # c_y_asst_lb = [
+    #     @constraint(Lower(m), sum(y[i, j] for j in 1:num_units) >= 1)
+    #     for i = 1:num_units
+    # ]
+    # for i = 1:num_units
+    #     set_name(c_x_asst_ub[i], "c_x_asst_ub[$(i)]")
+    #     set_name(c_x_asst_lb[i], "c_x_asst_lb[$(i)]")
+    #     set_name(c_y_asst_ub[i], "c_y_asst_ub[$(i)]")
+    #     set_name(c_y_asst_lb[i], "c_y_asst_lb[$(i)]")
+    # end
+
+    # @constraint(Lower(m), c_y7_atmost_one, y[7, 7] + y[7, 11] + y[7, 14] <= 1)
+
+    # @constraint(Lower(m), sum(y[i, j] for i in 1:num_units, j in 1:num_units) >= 16) # TEST, TODO: remove
 
     # Add population balance constraints
-    popbal_tol = 1.0e-4
-    x_ideal_district_pop = num_units ÷ (num_districts * 2)
-    x_popbal_lb = x_ideal_district_pop * (1.0 - popbal_tol)
-    x_popbal_ub = x_ideal_district_pop * (1.0 + popbal_tol)
-    y_ideal_district_pop = num_units ÷ num_districts
-    y_popbal_lb = y_ideal_district_pop * (1.0 - popbal_tol)
-    y_popbal_ub = y_ideal_district_pop * (1.0 + popbal_tol)
+    # popbal_tol = 1.0e-4
+    # x_ideal_district_pop = num_units ÷ (num_districts * 2)
+    # x_popbal_lb = x_ideal_district_pop * (1.0 - popbal_tol)
+    # x_popbal_ub = x_ideal_district_pop * (1.0 + popbal_tol)
+    # y_ideal_district_pop = num_units ÷ num_districts
+    # y_popbal_lb = y_ideal_district_pop * (1.0 - popbal_tol)
+    # y_popbal_ub = y_ideal_district_pop * (1.0 + popbal_tol)
+    # @printf "subdistrict pop bounds: %.4f to %.4f\n" x_popbal_lb x_popbal_ub
+    # @printf "District    pop bounds: %.4f to %.4f\n" y_popbal_lb y_popbal_ub
     
-    for j = 1:num_units
-        continue
-        # @constraint(Upper(m), sum(x[i, j] for i in 1:num_units) <= x_popbal_ub)
-        # @constraint(Upper(m), sum(x[i, j] for i in 1:num_units) >= x_popbal_lb)
-        # @constraint(Lower(m), sum(y[i, j] for i in 1:num_units) <= y_popbal_ub)
-        # @constraint(Lower(m), sum(y[i, j] for i in 1:num_units) >= y_popbal_lb)
-    end
+    
+    # c_x_popbal_ub = [
+    #     @constraint(Upper(m), sum(x[i, j] for i in 1:num_units) <= x_popbal_ub * x[j, j])
+    #     for j = 1:num_units
+    # ]
+    # c_x_popbal_lb = [
+    #     @constraint(Upper(m), sum(x[i, j] for i in 1:num_units) >= x_popbal_lb * x[j, j])
+    #     for j = 1:num_units
+    # ]
+    # c_y_popbal_ub = [
+    #     @constraint(Lower(m), sum(y[i, j] for i in 1:num_units) <= y_popbal_ub * y[j, j])
+    #     for j = 1:num_units
+    # ]
+    # c_y_popbal_lb = [
+    #     @constraint(Lower(m), sum(y[i, j] for i in 1:num_units) >= y_popbal_lb * y[j, j])
+    #     for j = 1:num_units
+    # ]
+    # for j = 1:num_units
+    #     set_name(c_x_popbal_ub[j], "c_x_popbal_ub[$(j)]")
+    #     set_name(c_x_popbal_lb[j], "c_x_popbal_lb[$(j)]")
+    #     set_name(c_y_popbal_ub[j], "c_y_popbal_ub[$(j)]")
+    #     set_name(c_y_popbal_lb[j], "c_y_popbal_lb[$(j)]")
+    # end
+    
 
     # Add some symmetry-breaking constraints. 
     # TODO: Add L-fixing/pos-based symmetry breaking. 
-    for i = 1:num_units
-        for j = i+1:num_units
-            @constraint(Upper(m), x[i, j] <= 0)
-        end
-    end
+    # for i = 1:num_units
+    #     for j = i+1:num_units
+    #         @constraint(Upper(m), x[i, j] <= 0)
+    #     end
+    # end
     
-    for i = 1:num_units
-        for j = i+1:num_units
-            @constraint(Lower(m), y[i, j] <= 0)
-        end
-    end
+    # for i = 1:num_units
+    #     for j = i+1:num_units
+    #         @constraint(Lower(m), y[i, j] <= 0)
+    #     end
+    # end
 
     # Add constraints that y assignments follow x assignments: 
     # If x_ij = 1, then y_ik <= y_jk for all units i, j, k. 
+    
     # for k = 1:num_units
     #     for i = 1:num_units
     #         for j = 1:num_units
@@ -269,19 +356,19 @@ function build_and_solve_grid_model(num_rows, num_cols, num_districts, voter_gri
     # end
 
     # Get directed edge set
-    dir_edge_list = get_directed_grid_edges(num_rows, num_cols)
-    num_dir_edges = length(dir_edge_list)
-    # Get undirected edges (u, v) in canonical order (u < v)
-    undir_edge_list = [(e[1], e[2]) for e in dir_edge_list if e[1] < e[2]]
-    num_undir_edges = length(undir_edge_list)
-    @printf "Directed edges:  \t %i\n" num_dir_edges
-    @printf "Undirected edges:\t %i\n" num_undir_edges
-    # display(undir_edge_list)
-    println()
+    # dir_edge_list = get_directed_grid_edges(num_rows, num_cols)
+    # num_dir_edges = length(dir_edge_list)
+    # # Get undirected edges (u, v) in canonical order (u < v)
+    # undir_edge_list = [(e[1], e[2]) for e in dir_edge_list if e[1] < e[2]]
+    # num_undir_edges = length(undir_edge_list)
+    # @printf "Directed edges:  \t %i\n" num_dir_edges
+    # @printf "Undirected edges:\t %i\n" num_undir_edges
+    # # display(undir_edge_list)
+    # println()
 
     # Add Shirabe flow variables for contiguity
     # TODO: It seems these have to be integral for MibS to be able to solve the bilevel MILP... 
-    @variable(Lower(m), f[1:num_units, 1:num_dir_edges] >= 0, Int)
+    # @variable(Lower(m), f[1:num_units, 1:num_dir_edges] >= 0, Int)
 
     # Add cut edge variables with quadratic constraints to define them
     # TODO: May have to manually linearize these quadratic constraints if MibS is unhappy
@@ -331,24 +418,101 @@ solution = BilevelJuMP.solve_with_MibS(model, MibS_jll.mibs, verbose_results=tru
 @printf "Status: %s\n" solution.status
 @printf "Definer opt obj.: %.1f\n" solution.objective
 
-asst = Dict() # y, or Combiner, assignment
+x_asst = Dict() # x, or Definer, assignment
+y_asst = Dict() # y, or Combiner, assignment
 
-for i in 1:num_rows
-    for j in 1:num_cols
-        curr_index = rowcol_to_index(i - 1, j - 1, num_cols) + 1
-        for center_index in 1:num_units
-            varname = string("y[", curr_index, ",", center_index, "]")
-            if solution.all_lower[varname] ≈ 1
-                asst[curr_index] = center_index
-            end
+dist_centers = Set()
+subdist_centers = Set()
+
+x_sum = 0.0
+y_sum = 0.0
+y7_sum = 0.0
+
+for i in 1:num_units
+    for j in 1:num_units
+        y_varname = string("y[", i, ",", j, "]")
+        if solution.all_lower[y_varname] ≈ 1
+            push!(dist_centers, j)
         end
     end
 end
 
-for i in 1:num_rows
-    for j in 1:num_cols
-        curr_index = rowcol_to_index(i - 1, j - 1, num_cols) + 1
-        @printf "%3i" asst[curr_index]
-    end
-    println()
+# TODO: I think the bug is actually in here, where I try to convert/recover the indices for assignments
+# for i in 1:num_rows
+#     for j in 1:num_cols
+#         curr_index = rowcol_to_index(i - 1, j - 1, num_cols) + 1
+#         for center_index in 1:num_units
+#             x_varname = string("x[", curr_index, ",", center_index, "]")
+#             if solution.all_upper[x_varname] ≈ 1
+#                 x_asst[curr_index] = center_index
+#                 if curr_index == center_index
+#                     push!(subdist_centers, curr_index)
+#                 end
+#             end
+
+#             y_varname = string("y[", curr_index, ",", center_index, "]")
+#             global y_sum += solution.all_lower[y_varname]
+#             if curr_index == 7
+#                 global y7_sum += solution.all_lower[y_varname]
+#             end
+            
+#             if solution.all_lower[y_varname] > 0 # ≈ 1
+#                 @printf "%3i is assigned to %3i by combiner: (%3.2f)\n" curr_index center_index solution.all_lower[y_varname]
+#                 y_asst[curr_index] = center_index
+#                 if curr_index == center_index
+#                     push!(dist_centers, curr_index)
+#                 end
+#             end
+#         end
+#     end
+# end
+
+@printf "Sum of y variables: %.1f\n" y_sum
+@printf "Sum of y[7, j] variables: %.1f\n" y7_sum
+
+println("\n*** Definer centers ***")
+for unit in subdist_centers
+    @printf "%2i, a.k.a. (%2i, %2i), is a subdistrict center.\n" unit index_to_rowcol_onebased(unit, num_cols)...
 end
+
+println("\n*** Definer assignment ***")
+# for i in 1:num_rows
+#     for j in 1:num_cols
+#         curr_index = rowcol_to_index(i - 1, j - 1, num_cols) + 1
+#         @printf "%3i" x_asst[curr_index]
+#     end
+#     println()
+# end
+
+println("\n*** Combiner centers ***")
+for unit in dist_centers
+    @printf "%2i, a.k.a. (%2i, %2i), is a district center.\n" unit index_to_rowcol_onebased(unit, num_cols)...
+end
+
+println("\n*** Combiner assignment ***")
+# for i in 1:num_rows
+#     for j in 1:num_cols
+#         curr_index = rowcol_to_index(i - 1, j - 1, num_cols) + 1
+#         assigned_center = haskey(y_asst, curr_index) ? string(y_asst[curr_index]) : "?"
+#         @printf "%3s" assigned_center
+#     end
+#     println()
+# end
+
+# println(solution)
+nz_lower = solution.nonzero_lower
+println(nz_lower)
+println(length(nz_lower))
+
+for low_name in keys(solution.all_lower)
+    # @printf "%s = %.2f\n" low_name solution.all_lower[low_name]
+    if solution.all_lower[low_name] > 0
+        @printf "%s is a nonzero lower var: %.2f\n" low_name solution.all_lower[low_name]
+    end
+end
+
+# for nz_low_key in keys(nz_lower)
+#     println(nz_low_key)
+#     println(solution.all_lower[nz_low_key])
+# end
+
---
+++ definecombine/mibs_bug_variable_array.jl
@@ -0,0 +1,82 @@
+# Demonstration of variable arrays bug in BilevelJuMP with MibS solver. 
+
+# Model of the problem
+# First level
+# ```math
+# \max_{{\bf x}} \sum_{i,j} x_{i,j},\\
+# \notag s.t.\\
+# x \in \{0, 1\}^{n \times ^n},\\
+# ```
+# Second level
+# ```math
+# \max_{{\bf y}} \sum_{i,j} y_{i,j},\\
+# \notag s.t.\\
+# \sum_{j \in [n]} y_{j,j} == 1,\\
+# y \in \{0, 1\}^{n \times ^n}.\\
+# ```
+
+using BilevelJuMP
+using MibS_jll
+using Printf
+
+function build_model(num_units, center_index)
+    # Create model and assignment variables
+    m = BilevelModel()
+    @variable(Upper(m), x[1:num_units, 1:num_units], Bin)
+    @variable(Lower(m), y[1:num_units, 1:num_units], Bin)
+
+    # Add max-sum objectives
+    @objective(Upper(m), Max, sum(x[j, j] for j in 1:num_units))
+    @objective(Lower(m), Max, sum(y[j, j] for j in 1:num_units))
+
+    # Add sum(y_jj) == 1, i.e., one unit is chosen as the center
+    @constraint(Lower(m), c_num_dist_ub, sum(y[j, j] for j in 1:num_units) <= 1)
+    @constraint(Lower(m), c_num_dist_lb, sum(y[j, j] for j in 1:num_units) >= 1)
+
+    # Force the unit labeled center_index to be the center
+    @constraint(Lower(m), c_only_center, y[center_index, center_index] >= 1)
+
+    # Force all other units to NOT be the center
+    for i = 1:num_units
+        if i != center_index
+            @constraint(Lower(m), y[i, i] <= 0)
+        end
+    end
+    
+    return m
+end
+
+
+### For each possible center_index, build and solve model using MibS
+num_units = 16
+
+for c_i = 1:num_units
+    model = build_model(num_units, c_i)
+    # display(model)
+    solution = BilevelJuMP.solve_with_MibS(model, MibS_jll.mibs)
+    for low_name in keys(solution.all_lower)
+        if solution.all_lower[low_name] > 0
+            @printf "When center_index is %i, the only nonzero lower variable is %s = %.2f. Expected: y[%i, %i]\n" c_i low_name solution.all_lower[low_name] c_i c_i
+        end
+    end
+end
+
+### Output: 
+#=
+When center_index is 1, the only nonzero lower variable is y[13,4] = 1.00. Expected: y[1, 1]
+When center_index is 2, the only nonzero lower variable is y[5,16] = 1.00. Expected: y[2, 2]
+When center_index is 3, the only nonzero lower variable is y[10,13] = 1.00. Expected: y[3, 3]
+When center_index is 4, the only nonzero lower variable is y[6,1] = 1.00. Expected: y[4, 4]
+When center_index is 5, the only nonzero lower variable is y[15,9] = 1.00. Expected: y[5, 5]
+When center_index is 6, the only nonzero lower variable is y[12,10] = 1.00. Expected: y[6, 6]
+When center_index is 7, the only nonzero lower variable is y[5,2] = 1.00. Expected: y[7, 7]
+When center_index is 8, the only nonzero lower variable is y[12,2] = 1.00. Expected: y[8, 8]
+When center_index is 9, the only nonzero lower variable is y[1,11] = 1.00. Expected: y[9, 9]
+When center_index is 10, the only nonzero lower variable is y[4,3] = 1.00. Expected: y[10, 10]
+When center_index is 11, the only nonzero lower variable is y[13,7] = 1.00. Expected: y[11, 11]
+When center_index is 12, the only nonzero lower variable is y[7,10] = 1.00. Expected: y[12, 12]
+When center_index is 13, the only nonzero lower variable is y[1,9] = 1.00. Expected: y[13, 13]
+When center_index is 14, the only nonzero lower variable is y[10,3] = 1.00. Expected: y[14, 14]
+When center_index is 15, the only nonzero lower variable is y[13,3] = 1.00. Expected: y[15, 15]
+When center_index is 16, the only nonzero lower variable is y[15,5] = 1.00. Expected: y[16, 16]
+=#
